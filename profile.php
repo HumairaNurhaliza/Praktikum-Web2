a2d0b0f837383ecb87569b5a2d9beb31ff2d7d81
Humairanurhaliza <mairanhza@gmail.com>
2023-05-31 00:38:27 -0400

    Selesai Pertemuan 3

---
+++ profile.php
@@ -0,0 +1,45 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="text-center">
+            <img src="img/maira.jpg" alt="Humaira Nurhaliza" width="160" class="rounded-circle">
+            <table class="table mt-4" style="width:100%; margin:0 auto;">
+                <tbody>
+                    <tr>
+                        <td align="right" width="50%"><strong>NIM :</strong></td>
+                        <td align="left" width="50%">2110010063</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Fakultas / Program Studio :</strong></td>
+                        <td align="left" width="50%">Teknologi Informasi / Teknik Informatika</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Nama Lengkap :</strong></td>
+                        <td align="left" width="50%">Humaira Nurhaliza</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Tempat, Tanggal Lahir :</strong></td>
+                        <td align="left" width="50%">Banjarmasin, 22 Maret 2003</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Telepon :</strong></td>
+                        <td align="left" width="50%">0882 4588 0823</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>email :</strong></td>
+                        <td align="left" width="50%">mairanhza@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Media Sosial :</strong></td>
+                        <td align="left" width="50%">                         
+                            <a href="http://www.instagram.com/real__maira/" target=" _blank" style="text-decoration:none;">
+                                <img src="img/instagram.svg" width="30" alt="instagram">
+                            </a>
+
+                            </a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
